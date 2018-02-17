583ab289245234d009f9c9a2cccd2d546eac826f
kooldrmony <derrickreyes.23@gmail.com>
2018-02-17 10:15:03 -0700

    updated PORT from port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -27,5 +27,5 @@ var routes = require("./controllers/burgers_controller.js");
 app.use("/", routes);
 
 app.listen(port, function() {
-  console.log("Listening on PORT " + port);
+  console.log("Listening on PORT " + PORT);
 });
